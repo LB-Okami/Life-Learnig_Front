6f08b47bc0d0c92c2418e4ccab4e08611230c587
Lucas Buzo <lucabtei@outlook.com>
2021-05-28 15:56:12 -0300

    final-merge

--- Life-Learning/src/app/app-routing.module.ts
+++ Life-Learning/src/app/app-routing.module.ts
@@ -1,11 +1,24 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './menu/menu.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { SobreComponent } from './sobre/sobre.component';
+import { EntrarComponent } from './entrar/entrar.component';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { InicioComponent } from './inicio/inicio.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full' },
 
-  {path:'home', component: HomeComponent}
+  {path:'home', component: HomeComponent},
+  {path:'cadastro', component: CadastrarComponent},
+  {path:'logar', component: EntrarComponent},
+  {path:'sobre', component: SobreComponent},
+  {path:'rodape', component: RodapeComponent},
+  {path:'menu', component: MenuComponent},
+  {path: 'inicio', component: InicioComponent},
+
 ];
 
 @NgModule({
--- Life-Learning/src/app/app.module.ts
+++ Life-Learning/src/app/app.module.ts
@@ -5,12 +5,23 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { RodapeComponent } from './rodape/rodape.component';
+import { HomeComponent } from './home/home.component';
+import { SobreComponent } from './sobre/sobre.component';
+import { EntrarComponent } from './entrar/entrar.component';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { InicioComponent } from './inicio/inicio.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    RodapeComponent
+    RodapeComponent,
+    HomeComponent,
+    SobreComponent,
+    EntrarComponent,
+    CadastrarComponent,
+    InicioComponent
+
   ],
   imports: [
     BrowserModule,
