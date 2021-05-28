119f35a9de2a6ba38d68007f3e2cf1c6d53986f3
Lucas Buzo <lucabtei@outlook.com>
2021-05-28 15:10:36 -0300

    footer-fix

--- Life-Learning/src/app/app.module.ts
+++ Life-Learning/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { TesteComponent } from './teste/teste.component';
 
 @NgModule({
-  declarations: [	
+  declarations: [		
     AppComponent,
-      HomeComponent
+      HomeComponent,
+      TesteComponent
    ],
   imports: [
     BrowserModule,
