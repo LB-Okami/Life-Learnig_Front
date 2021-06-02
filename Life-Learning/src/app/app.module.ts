7f7995f33ec13316ec0f3801530330da85514923
hashtagmath <matheussilvadossantos0@gmail.com>
2021-06-02 12:00:54 -0300

    a finalizar

--- Life-Learning/src/app/app.module.ts
+++ Life-Learning/src/app/app.module.ts
@@ -1,6 +1,9 @@
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http'
+import { FormsModule } from '@angular/forms';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -27,8 +30,12 @@ import { InicioComponent } from './inicio/inicio.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
+  
   ],
+
+  
   providers: [],
   bootstrap: [AppComponent]
 })
